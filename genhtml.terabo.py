39c96ac5c833ba77e0fe4db71beed191849a2b82
Kotaro Terada <tera@toki.waseda.jp>
2016-12-09 16:43:09 +0900

    Copy to genhtml.terabo.py

---
+++ genhtml.terabo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('.', encoding='utf-8'))
+tpl = env.get_template('champly.tpl')
+param = {}
+
+#### HTMLを生成するための設定項目 ####
+# サイト名
+param['sitename'] = 'terabo.net'
+# ベースURL
+param['baseurl'] = 'http://www.terabo.net/champly'
+# パンくずリスト
+param['breadcrumb'] = '<a href="/">Top</a> <i class="icon-angle-right"></i> <a href="./champly">Champly!</a>'
+# Googleアナリティクス
+param['ga'] = 'UA-56685700-1'
+################################
+
+print(tpl.render(param=param))
